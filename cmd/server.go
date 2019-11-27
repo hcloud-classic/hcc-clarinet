6626c0099e35cd1665357606bdf48b28a58b48d2
ish <ish@innogrid.com>
2019-11-27 22:56:40 +0900

    cmd: server: Add create option

--- cmd/server.go
+++ cmd/server.go
@@ -17,6 +17,42 @@ var ServerCmd = &cobra.Command{
 	Args: cobra.MinimumNArgs(1),
 }
 
+var subnetUUID string
+var _os string
+var serverName string
+var serverDesc string
+var cpu int
+var memory int
+var diskSize int
+var userUUID string
+var nrNode int
+
+var serverCreate = &cobra.Command{
+	Use:   "create",
+	Short: "Create server.",
+	Long: `Create server.`,
+	Args: cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		queryArgs := make(map[string]interface{})
+		queryArgs["subnet_uuid"] = subnetUUID
+		queryArgs["os"] = _os
+		queryArgs["server_name"] = serverName
+		queryArgs["server_desc"] = serverDesc
+		queryArgs["cpu"] = cpu
+		queryArgs["memory"] = memory
+		queryArgs["disk_size"] = diskSize
+		queryArgs["user_uuid"] = userUUID
+		queryArgs["nr_node"] = nrNode
+		server, err := mutationParser.CreateServer(queryArgs)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Creating server for UUID: " + server.(model.Server).UUID)
+	},
+}
+
 var row int
 var page int
 
@@ -24,7 +60,7 @@ var serverList = &cobra.Command{
 	Use:   "list",
 	Short: "Get list of servers with row and page options.",
 	Long: `Get list of servers with row and page options.`,
-	Args: cobra.MaximumNArgs(0),
+	Args: cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
 		queryArgs := make(map[string]interface{})
 		if row != 0 || page != 0 {
@@ -72,7 +108,7 @@ var serverDelete = &cobra.Command{
 	Use:   "delete",
 	Short: "Delete one of server by UUID.",
 	Long: `Delete one of server by UUID.`,
-	Args: cobra.MaximumNArgs(0),
+	Args: cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
 		queryArgs := make(map[string]interface{})
 		queryArgs["uuid"] = uuid
@@ -87,10 +123,20 @@ var serverDelete = &cobra.Command{
 }
 
 func ReadyServerCmd() {
+	serverCreate.Flags().StringVar(&subnetUUID, "subnet_uuid", "", "UUID of subnet")
+	serverCreate.Flags().StringVar(&_os, "os", "", "Type of OS")
+	serverCreate.Flags().StringVar(&serverName, "server_name", "", "Name of server")
+	serverCreate.Flags().StringVar(&serverDesc, "server_desc", "", "Description of server")
+	serverCreate.Flags().IntVar(&cpu, "cpu", 0, "Number of CPU cores")
+	serverCreate.Flags().IntVar(&memory, "memory", 0, "Size of memory")
+	serverCreate.Flags().IntVar(&diskSize, "disk_size", 0, "Size of disk")
+	serverCreate.Flags().StringVar(&userUUID, "user_uuid", "", "UUID of user")
+	serverCreate.Flags().IntVar(&nrNode, "nr_node", 0, "Number of nodes")
+
 	serverList.Flags().IntVar(&row, "row", 0, "rows of server list")
 	serverList.Flags().IntVar(&page, "page", 0, "page of server list")
 
 	serverDelete.Flags().StringVar(&uuid, "uuid", "", "UUID of server")
 
-	ServerCmd.AddCommand(serverList, serverDelete)
+	ServerCmd.AddCommand(serverCreate, serverList, serverDelete)
 }
