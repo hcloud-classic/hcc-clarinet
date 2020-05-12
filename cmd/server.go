162363244969e46e0a568265554ad1701157cc77
ish <ish@innogrid.com>
2020-05-12 12:30:50 +0900

    [Hcloud-MiddleWare] Clarinet Command Line builder
    String parser를 포함해 각 서버 제어 커맨드 빌더 구현 완료

--- cmd/server.go
+++ cmd/server.go
@@ -3,8 +3,11 @@ package cmd
 import (
 	"fmt"
 	"hcc/clarinet/action/graphql/mutationParser"
+	"hcc/clarinet/action/graphql/queryParser"
 	"hcc/clarinet/model"
+	"os"
 
+	"github.com/jedib0t/go-pretty/table"
 	"github.com/spf13/cobra"
 )
 
@@ -53,3 +56,91 @@ var serverCreate = &cobra.Command{
 
 var row int
 var page int
+
+var serverList = &cobra.Command{
+	Use:   "list",
+	Short: "Get list of servers with row and page options.",
+	Long:  `Get list of servers with row and page options.`,
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		queryArgs := make(map[string]interface{})
+		if row != 0 || page != 0 {
+			if row <= 0 || page <= 0 {
+				fmt.Println("Please provide row and page options correctly.")
+				return
+			}
+			queryArgs["row"] = row
+			queryArgs["page"] = page
+		}
+		servers, err := queryParser.AllServer(queryArgs)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		serverNum, err := queryParser.NumServer()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"No", "UUID", "Server Name", "Cores", "Memory", "Disk", "Nodes", "Status"})
+		for i, server := range servers.([]model.Server) {
+			if server.Status == "Deleted" {
+				continue
+			}
+			serverUUIDArg := make(map[string]interface{})
+			serverUUIDArg["server_uuid"] = server.UUID
+			numNodesServer, err := queryParser.NumNodesServer(serverUUIDArg)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			t.AppendRow([]interface{}{i + 1, server.UUID, server.ServerName, 16, 16 /*server.CPU, server.Memory*/, server.DiskSize,
+				numNodesServer.(model.ServerNodeNum).Number, server.Status})
+		}
+		t.AppendFooter(table.Row{"Total Server Num", serverNum.(model.ServerNum).Number, "", "", "", "", "", ""})
+		t.Render()
+	},
+}
+
+var uuid string
+
+var serverDelete = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete one of server by UUID.",
+	Long:  `Delete one of server by UUID.`,
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		queryArgs := make(map[string]interface{})
+		queryArgs["uuid"] = uuid
+		_, err := mutationParser.DeleteServer(queryArgs)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Delete command ended successfully.")
+	},
+}
+
+func ReadyServerCmd() {
+	serverCreate.Flags().StringVar(&subnetUUID, "subnet_uuid", "", "UUID of subnet")
+	serverCreate.Flags().StringVar(&_os, "os", "", "Type of OS")
+	serverCreate.Flags().StringVar(&serverName, "server_name", "", "Name of server")
+	serverCreate.Flags().StringVar(&serverDesc, "server_desc", "", "Description of server")
+	serverCreate.Flags().IntVar(&cpu, "cpu", 0, "Number of CPU cores")
+	serverCreate.Flags().IntVar(&memory, "memory", 0, "Size of memory")
+	serverCreate.Flags().IntVar(&diskSize, "disk_size", 0, "Size of disk")
+	serverCreate.Flags().StringVar(&userUUID, "user_uuid", "", "UUID of user")
+	serverCreate.Flags().IntVar(&nrNode, "nr_node", 0, "Number of nodes")
+
+	serverList.Flags().IntVar(&row, "row", 0, "rows of server list")
+	serverList.Flags().IntVar(&page, "page", 0, "page of server list")
+
+	serverDelete.Flags().StringVar(&uuid, "uuid", "", "UUID of server")
+
+	ServerCmd.AddCommand(serverCreate, serverList, serverDelete)
+}
