7438459ecdaa91cee4bf4f93fb2a518271cec0f8
ish <ish@innogrid.com>
2020-05-19 10:10:32 +0900

    [Hcloud-MiddleWare] Clarinet Command Line http consumer
    * Node Control
      - NodeData(Node info)
      - AllNodeData
      - NumNodeData(Specific node)

---
+++ http/httpRequest.go
@@ -0,0 +1,104 @@
+package http
+
+import (
+	"encoding/json"
+	"errors"
+	clarinetData "hcc/clarinet/data"
+	"hcc/clarinet/lib/config"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// DoHTTPRequest : Send http request to other modules with GraphQL query string.
+func DoHTTPRequest(moduleName string, needData bool, dataType string, data interface{}, query string) (interface{}, error) {
+	var timeout time.Duration
+	var url = "http://"
+	switch moduleName {
+	case "flute":
+		timeout = time.Duration(config.Flute.RequestTimeoutMs)
+		url += config.Flute.ServerAddress + ":" + strconv.Itoa(int(config.Flute.ServerPort))
+		break
+	case "harp":
+		timeout = time.Duration(config.Harp.RequestTimeoutMs)
+		url += config.Harp.ServerAddress + ":" + strconv.Itoa(int(config.Harp.ServerPort))
+		break
+	case "violin":
+		timeout = time.Duration(config.Violin.RequestTimeoutMs)
+		url += config.Violin.ServerAddress + ":" + strconv.Itoa(int(config.Violin.ServerPort))
+		break
+	default:
+		return nil, errors.New("unknown module name")
+	}
+	url += "/graphql?query=" + queryURLEncoder(query)
+
+	client := &http.Client{Timeout: timeout * time.Millisecond}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+		// Check response
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err == nil {
+			result := string(respBody)
+
+			if strings.Contains(result, "errors") {
+				return nil, errors.New(result)
+			}
+
+			if needData {
+				if data == nil {
+					return nil, errors.New("needData marked as true but data is nil")
+				}
+
+				switch dataType {
+				case "NodeData":
+					nodeData := data.(clarinetData.NodeData)
+					err = json.Unmarshal([]byte(result), &nodeData)
+					if err != nil {
+						return nil, err
+					}
+
+					return nodeData.Data.Node, nil
+				case "AllNodeData":
+					allNodeData := data.(clarinetData.AllNodeData)
+					err = json.Unmarshal([]byte(result), &allNodeData)
+					if err != nil {
+						return nil, err
+					}
+
+					return allNodeData.Data.AllNode, nil
+				case "NumNodeData":
+					numNodeData := data.(clarinetData.NumNodeData)
+					err = json.Unmarshal([]byte(result), &numNodeData)
+					if err != nil {
+						return nil, err
+					}
+
+					return numNodeData.Data.NumNode, nil
+
+				default:
+					return nil, errors.New("unknown data type")
+				}
+			}
+
+			return result, nil
+		}
+
+		return nil, err
+	}
+
+	return nil, errors.New("http response returned error code")
+}
